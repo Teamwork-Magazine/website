d24c7b8d875815c5f3ba8d422eb7a2948dfb8e16
Cameron Messinides <cameron.messinides@gmail.com>
2021-08-18 21:53:28 -0400

    Remove experimental network tracing

--- prismic/client.ts
+++ prismic/client.ts
@@ -2,12 +2,6 @@ import Prismic from "@prismicio/client";
 import type { IncomingMessage } from "http";
 import { apiEndpoint, accessToken } from "./config";
 
-const __fetch = fetch;
-global.fetch = function tracedFetch(...args) {
-	console.log("fetch:", ...args);
-	return __fetch(...args);
-};
-
 export function createClient(req?: IncomingMessage) {
 	return Prismic.client(apiEndpoint, {
 		...(accessToken && { accessToken }),
