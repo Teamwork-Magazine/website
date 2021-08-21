6d40bb7d7137857dfc167c5a1d5a37ca1d33800e
Cameron Messinides <cameron.messinides@gmail.com>
2021-08-21 12:31:06 -0400

    Change Safari pinned tab color to be more consistent with other browsers

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -30,7 +30,7 @@ class CustomDocument extends Document {
 						href="/favicon-16x16.png"
 					/>
 					<link rel="manifest" href="/site.webmanifest" />
-					<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#cd3a0c" />
+					<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
 					<meta name="msapplication-TileColor" content="#da532c" />
 					<meta name="theme-color" content="#cd3a0c" />
 				</Head>
