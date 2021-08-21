7e1204bbce2aabe1a339b7b53a0963e64c9d1d66
Cameron Messinides <cameron.messinides@gmail.com>
2021-08-21 12:25:39 -0400

    Rebrand placeholder, favicon, and other icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,8 +10,31 @@ class CustomDocument extends Document {
 						href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Spectral:ital,wght@0,300;0,400;0,500;0,700;1,400;1,700&display=swap"
 						rel="stylesheet"
 					/>
+
+					{/* Favicon and other icons */}
+					<link
+						rel="apple-touch-icon"
+						sizes="180x180"
+						href="/apple-touch-icon.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="32x32"
+						href="/favicon-32x32.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="16x16"
+						href="/favicon-16x16.png"
+					/>
+					<link rel="manifest" href="/site.webmanifest" />
+					<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#cd3a0c" />
+					<meta name="msapplication-TileColor" content="#da532c" />
+					<meta name="theme-color" content="#cd3a0c" />
 				</Head>
-				<body className="font-body leading-normal">
+				<body>
 					<Main />
 					<NextScript />
 				</body>
